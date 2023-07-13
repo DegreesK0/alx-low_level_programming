d2c844118057c6cf17f53f3e9ff1cb5a551b90c0
DegreesK0 <kelvinbora@gmail.com>
2023-07-13 19:35:44 +0000

    More complicated shell tricks

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/*Prints out some text. Woohoo!!*/
+
+int main() {
+
+  /*char str[] = "Programming is like building a multilingual puzzle";*/
+
+  puts("Programming is like building a multilingual puzzle");
+
+  return 0;
+}
