e75c7e72c5da5a7cabab0e236b7b44e93bddb5e8
DegreesK0 <kelvinbora@gmail.com>
2023-07-13 20:24:27 +0000

    More complicated shell tricks

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - Prints out a function
+*
+* Return: 0 if no errors
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+
+	return (0);
+}
