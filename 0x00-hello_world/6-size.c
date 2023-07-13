664157a89ba40b3eec5df175f6329f535edc58d9
DegreesK0 <kelvinbora@gmail.com>
2023-07-13 21:30:00 +0000

    More complicated shell tricks

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - Prints the size of various types
+*
+* Return: 0 means code works
+*/
+int main(void)
+{
+	printf("Size of a char: 1 byte(s)")
+	printf("Size of an int: 4 byte(s)")
+	printf("Size of a long int: 4 byte(s)")
+	printf("Size of a long long int: 8 byte(s)")
+	printf("Size of a float: 4 byte(s)");
+
+	return (0);
+}
