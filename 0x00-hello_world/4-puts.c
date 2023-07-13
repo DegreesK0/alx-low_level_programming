874dd7ddd35ebeafbc6fa8f7b10a590a83bd70df
DegreesK0 <kelvinbora@gmail.com>
2023-07-13 19:56:10 +0000

    More complicated shell tricks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
-/*Prints out some text. Woohoo!!*/
-int main(void)
+int main(void) /*Prints out some text. Woohoo!!*/
 {
 	/*char str[] = "Programming is like building a multilingual puzzle";*/
 
