b046e88910ded4ade63c01e83d5be5edae88467f
DegreesK0 <kelvinbora@gmail.com>
2023-07-13 20:04:23 +0000

    More complicated shell tricks

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 
-
 /**
  * int main(void) - Prints out some text. Woohoo!!
+ *
+ * Return: 0 if function runs
  */
-int main(void) 
+int main(void)
 {
 	/*char str[] = "Programming is like building a multilingual puzzle";*/
 
