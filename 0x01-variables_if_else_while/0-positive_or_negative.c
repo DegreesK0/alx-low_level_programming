9c24c1b48bbf76ce648d6f6417fec1279f82a331
DegreesK0 <kelvinbora@gmail.com>
2023-07-14 11:56:59 +0000

    if-else functions in C

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+#include <stdio.h>
+
+/* betty style doc for function main goes there */
+/**
+ * main - Assigns a random number to n, and then declares
+ *
+ * Description: Assigns a random number to n,
+ * And then declares if it is postive, 0 or negative.
+ *
+ * Return: 0 for no error once done
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	if (n > 0)
+	{
+		printf("%d is positve\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is 0\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0);
+}
