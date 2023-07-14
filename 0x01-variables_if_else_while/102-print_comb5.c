5c4b2f418bdf5a05147a802f07dc8aac3402e456
DegreesK0 <kelvinbora@gmail.com>
2023-07-14 23:27:56 +0000

    if-else functions in C

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,20 +13,20 @@ int main(void)
 	int digit3;
 	int digit4;
 
-	for (digit1 = 0; digit1 <= 9; digit1++)
+	for (digit1 = '0'; digit1 <= '9'; digit1++)
 	{
-		for (digit2 = 0; digit2 <= 9; digit2++)
+		for (digit2 = '0'; digit2 <= '9'; digit2++)
 		{
-			for (digit3 = 0; digit3 <= 9; digit3++)
+			for (digit3 = '0'; digit3 <= '9'; digit3++)
 			{
-				for (digit4 = digit2 + 1; digit4 <= 9; digit4++)
+				for (digit4 = digit2 + 1; digit4 <= '9'; digit4++)
 				{
 					putchar(digit1);
 					putchar(digit2);
 					putchar(' ');
 					putchar(digit3);
 					putchar(digit4);
-					if (digit1 != 9 || digit2 != 8 || digit3 != 9 || digit4 != 9)
+					if (digit1 != '9' || digit2 != '8' || digit3 != '9' || digit4 != '9')
 					{
 						putchar(',');
 						putchar(' ');
