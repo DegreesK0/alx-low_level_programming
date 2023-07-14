a7a8c96bc248c4357ca81a0e66e70038e4fdb441
DegreesK0 <kelvinbora@gmail.com>
2023-07-14 21:30:41 +0000

    if-else functions in C

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,12 +11,15 @@ int main(void)
 	int digit1 = 48;
 	int digit2 = 49;
 
+	/* first for loop prints the first digit */
 	for (digit1 = 48; digit1 <= 57; digit1++)
 	{
+		/*Second loop prints the second digit. Always starts at 1 more than the first digit*/
 		for (digit2 = digit1 + 1; digit2 <= 57; digit2++)
 		{
 			putchar(digit1);
 			putchar(digit2);
+			/* Skips the last , and "" at the end of 89 */
 			if (digit1 != 56 || digit2 != 57)
 			{
 				putchar(',');
@@ -28,4 +31,7 @@ int main(void)
 	putchar('\n');
 
 	return (0);
+
 }
+
+
