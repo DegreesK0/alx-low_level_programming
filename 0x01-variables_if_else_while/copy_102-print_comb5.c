479512e2881f9fe450e15821133865f8576906aa
DegreesK0 <kelvinbora@gmail.com>
2023-07-15 00:56:45 +0000

    if-else functions in C

--- 0x01-variables_if_else_while/copy_102-print_comb5.c
+++ 0x01-variables_if_else_while/copy_102-print_comb5.c
@@ -25,15 +25,15 @@ int main(void)
 					}*/
 				for (digit4 = 48; digit4 <= 57; digit4++)
 				{
-					/*if (digit1 == 48 && digit2 == 48 && digit3 == 48 && digit4 == 48)
+					if (digit1 == 48 && digit2 == 48 && digit3 == 48 && digit4 == 48)
 					{
 						digit4 = 49;
-					}*/
+					}
 					/*if (digit4 < digit2)
 					{
 						digit4 = digit2;
 					}*/
-					if (digit3 > digit1)
+					if (digit1 + digit2 <= digit3 + digit4)
 					{
 						putchar(digit1);
 						putchar(digit2);
@@ -44,23 +44,6 @@ int main(void)
 						{
 							putchar(',');
 							putchar(' ');
-						}
-					}
-	 				else if (digit3 == digit4)
-					{
-						if (digit4 > digit2)
-						{
-							putchar(digit1);
-							putchar(digit2);
-							putchar(' ');
-							putchar(digit3);
-							putchar(digit4);
-							if (digit1 != 57 || digit2 != 56 || digit3 != 57 || digit4 != 57)
-							{
-								putchar(',');
-								putchar(' ');
-							}
-
 						}
 					}
 				}
