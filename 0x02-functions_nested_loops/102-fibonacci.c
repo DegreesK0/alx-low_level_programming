6eaad7c5490a5a33f49c2f72fc62d60f34f77ff6
DegreesK0 <kelvinbora@gmail.com>
2023-07-18 18:56:23 +0000

    Basics of Functions in C

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,12 +8,11 @@
 
 int main(void)
 {
-	int i = 0;
-	int num1 = 0;
-	int num2 = 1;
-	int sum = 0;
+	long i = 0;
+	long num1 = 0;
+	long num2 = 1;
+	long sum = 0;
 
-	printf("0, ");
 	for (i = 0; i < 50; i++)
 	{
 		sum = num1 + num2;
@@ -21,7 +20,11 @@ int main(void)
 		num1 = num2;
 		num2 = sum;
 
-		printf("%d, ", sum);
+		printf("%ld", sum);
+
+		if (i == 49)
+			break;
+		printf(", ");
 	}
 
 	printf("\n");
