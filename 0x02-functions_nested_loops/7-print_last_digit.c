2286e4cee73f2c6dcde746221670edf20810e5bb
DegreesK0 <kelvinbora@gmail.com>
2023-07-18 21:05:08 +0000

    Basics of Functions in C

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -7,7 +7,7 @@
 * Return: returns the last_digit
 */
 
-int print_last_digit(int num)
+/* int print_last_digit(int num)
 {
 	int last_digit = 0;
 
@@ -21,4 +21,14 @@ int print_last_digit(int num)
 	_putchar(last_digit + '0');
 
 	return (last_digit);
+}*/
+
+int print_last_digit(int n)
+{
+int last_digit;
+last_digit = n % 10;
+if (last_digit < 0)
+        last_digit *= -1;
+_putchar(last_digit + '0');
+return (last_digit);
 }
