5a1ac1338348d73d7832287cbf7ba85182f2d85d
DegreesK0 <kelvinbora@gmail.com>
2023-07-18 23:59:29 +0000

    Basics of Functions in C

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,7 +13,7 @@ int main(void)
 	long num1_half1 = 0, num1_half2 = 0, num2_half1 = 0, num2_half2 = 0;
 	long sum_half1 = 0, sum_half2 = 0;
 
-	for (i = 0; i < 4; i++)
+	for (i = 0; i < 90; i++)
 	{
 		sum = num1 + num2;
 		num1 = num2;
@@ -27,7 +27,7 @@ int main(void)
 	num2_half1 = num2 / 10000000000;
 	num2_half2 = num2 % 10000000000;
 
-	for (i = 90; i < 91; i++)
+	for (i = 90; i < 98; i++)
 	{
 		sum_half1 = num1_half1 + num2_half1;
 		sum_half2 = num1_half2 + num2_half2;
