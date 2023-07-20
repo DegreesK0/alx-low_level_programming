a61e67f0f1b1744ef39cbd775b3687a43b187c57
DegreesK0 <kelvinbora@gmail.com>
2023-07-20 19:34:38 +0000

    Fun Fun Function

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+* main - print out numbers 1 to 100, but replace multiples of 3 with Fizz, multiples of 5 with Buzz 
+* and multiples of 3 AND 5 with FizzBuzz
+*/
+
+int main(void)
+{
+	int num = 1;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else 
+		{
+			printf("%d ", num);
+		}
+	}
+
+	printf("\n");
+	
+	return (0);
+
+}
