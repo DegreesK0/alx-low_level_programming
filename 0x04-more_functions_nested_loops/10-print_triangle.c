3eed54e6f96c0e32bf6659ed7da7f94e75f8a0e5
DegreesK0 <kelvinbora@gmail.com>
2023-07-20 20:25:12 +0000

    Fun Fun Function

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+* print_triangle - prints out a triangle
+* @size: size of the triangle
+*/
+
+void print_triangle(int size)
+{
+
+
+	int i = 0;
+	int j = 0;
+	int k = 0;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		for (k = size - i - 1; k > 0; k--)
+		{
+			_putchar(' ');
+		}
+
+		for (j = 0; j <= i; j++)
+		{
+
+
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
