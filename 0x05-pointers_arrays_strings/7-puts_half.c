fcecab8b866c718a6bf35cbe08e371658dbdc54a
DegreesK0 <kelvinbora@gmail.com>
2023-07-24 21:28:42 +0000

    Upload

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,9 +15,20 @@ void puts_half(char *str)
 		i = i;
 	}
 
-	for (j = i / 2; j < i; j++)
+	if (i % 2 == 0)
 	{
-		_putchar(str[j]);
+		for (j = i / 2; j < i; j++)
+		{
+			_putchar(str[j]);
+		}
 	}
+	else if (i % 2 == 1)
+	{
+		for (j = i / 2 - 1; j < i; j++)
+		{
+			_putchar(str[j]);
+		}
+	}
+
 	_putchar('\n');
 }
