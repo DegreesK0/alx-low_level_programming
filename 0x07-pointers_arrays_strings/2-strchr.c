199abfe0b4db19071db39e518d631cee6dd8b225
DegreesK0 <kelvinbora@gmail.com>
2023-07-31 18:18:40 +0000

    Upload

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,24 +13,12 @@
 
 char *_strchr(char *s, char c)
 {
-	char *f = 0;
-	int len = 0;
-	int i = 0;
-	/* unsigned char *byte_s = (unsigned char *)s; */
-	/* unsigned char byte_c = (unsigned char) c; */
-
-	while (s[len] != '\0')
-		len++;
-
-	for (i = 0; i < len; i++)
+	while (*s != '\0')
 	{
-		if (s[i] == c)
-		{
-			f = &s[i];
-			return (f);
-		}
+		if (*s == c)
+			return ((char *)s);
+	s++;
 	}
 
-	f = NULL;
-	return (f);
+	return (NULL);
 }
