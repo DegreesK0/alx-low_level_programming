66a545367081d7104d9fd2c3d16e97f3ca89f79b
DegreesK0 <kelvinbora@gmail.com>
2023-07-31 19:59:30 +0000

    Upload

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _strstr - locates the first occurenece in the string haystack
+* of any bytes in the string needle
+* @haystack: string to search in
+* @needle: string to locate
+*
+* Return: a pointer to the beginning of the located substring
+* or NULL if no such byte is found
+*/
+
+char *_strstr(char *haystack, char *needle)
+{
+	char *str = haystack;
+	char *a = needle;
+
+	for (str = haystack; *str != '\0'; str++)
+	{
+		for (a = needle; *a != '\0'; a++)
+		{
+			if (*a == *str)
+			{
+				return ((char *)str);
+			}
+		}
+	}
+
+	return (NULL);
+}
+
+
