cc03369b85e654cedfe48a7679a69d7f8c13f03b
DegreesK0 <kelvinbora@gmail.com>
2023-08-02 23:05:32 +0000

    Upload

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* is_palindrome - checks if a string is a palindrome
+* @s: string to check
+*
+* Return: retrun 1 if a palindrome, 0 if not
+*/
+
+int is_palindrome(char *s)
+{
+	if (*s == i)
+	{
+		s++
+
+	}
+}
+
