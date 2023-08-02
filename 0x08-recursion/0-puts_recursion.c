1242394fc1154230771b2f609da23f4aa400aca2
DegreesK0 <kelvinbora@gmail.com>
2023-08-02 13:32:13 +0000

    Upload

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,19 +9,15 @@
 void _puts_recursion(char *s)
 {
 
-	_putchar(*s);
-
-	if (*s != '\0')
-	{
-		s++;
-		_puts_recursion(s);
-	}
-
 	if (*s == '\0')
 	{
 		_putchar('\n');
+		return;
 	}
 
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
 
 
