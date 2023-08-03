b593c14c8f44f14ad9f7fb3be520f5f0fc3e43d6
DegreesK0 <kelvinbora@gmail.com>
2023-08-03 18:01:28 +0000

    Upload

---
+++ 0x0A-argc_argv/1-arg.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - 
+ * @argv: number of arguments (argument count)
+ * @argv: the values of arguments (argument values)
+ * 
+ * Return: Always 0 on success
+ */
+
+
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments passed into it
+ * @argc: number of arguments (argument count)
+ * @argv: the values of arguments (argument values)
+ *
+ * Return: Always 0 on success
+ */
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
