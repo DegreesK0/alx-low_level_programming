8b082c6a53ceef68e485ca0378de262c4d364dbb
DegreesK0 <kelvinbora@gmail.com>
2023-08-08 15:08:09 +0000

    Upload

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,16 +14,20 @@ int **alloc_grid(int width, int height)
 {
 	int i = 0;
 	int j = 0;
+	int k = 0;
 	int **arr;
 
-	arr = malloc(sizeof(int) * height);
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	arr = malloc(sizeof(int *) * height);
 
 	if (arr == NULL)
 		return (NULL);
 
 	for (i = 0; i < height; i++)
 	{
-		arr[i] = malloc(sizeof(int) * (width));
+		arr[i] = malloc(sizeof(int) * width);
 
 		if (arr[i] == NULL)
 		{
@@ -36,7 +40,7 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 
-		for (j = 0; j < width; j++)
+		for (k = 0; k < width; k++)
 		{
 			arr[i][j] = 0;
 		}
