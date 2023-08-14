52ead399b47127ad15e0a6df4084b29ef420c372
DegreesK0 <kelvinbora@gmail.com>
2023-08-14 18:32:17 +0000

    Upload

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - data for dogs
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the name of the dog's owner
+ */
+
+
 struct dog
 {
 	char *name;
