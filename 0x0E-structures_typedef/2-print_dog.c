01ce780f98dddc0c978f4130e4e687f5469a582a
DegreesK0 <kelvinbora@gmail.com>
2023-08-14 19:05:23 +0000

    Upload

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ */
+
+void print_dog(struct dog *d)
+{
+	printf("Name: %s\n", (*d).name);
+	printf("Age: %f\n", (*d).age);
+	printf("Owner: %s\n", d->owner);
+}
+
