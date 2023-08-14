727f70772f254e226d6e468fcaffd7ea1b602987
DegreesK0 <kelvinbora@gmail.com>
2023-08-14 20:47:04 +0000

    Upload

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_dog - frees the space taking by dog
+ * @d: attributes for dog
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
