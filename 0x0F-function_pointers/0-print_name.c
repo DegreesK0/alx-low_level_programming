682ed4c777e2c818aca558a956bf99ebea8fd9d7
DegreesK0 <kelvinbora@gmail.com>
2023-08-16 12:26:22 +0000

    Upload

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - passes the name to another function to be printed
+ * @name: the name to be printed
+ * @f: the function to pass the name to.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
+
+
+
