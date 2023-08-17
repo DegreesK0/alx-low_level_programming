b27fb809692d8054f95f4b25a4bf44802f053176
DegreesK0 <kelvinbora@gmail.com>
2023-08-17 21:44:24 +0000

    Upload

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,23 +1,38 @@
-#include <stdio.h>
-#include <stdlib.h>
+/*#include <stdio.h> */
+/*#include <stdlib.h> */
+/*#include "function_pointers.h" */
+
+/*/1** */
+/* * print_name - passes the name to another function to be printed */
+/* * @name: the name to be printed */
+/* * @f: the function to pass the name to. */
+/* *1/ */
+
+/*void print_name(char *name, void (*f)(char *)) */
+/*{ */
+/*	if (name == NULL || f == NULL) */
+/*		return; */
+
+/*	/1* if (!name || !f) *1/ */
+/*	/1*	return; *1/ */
+
+/*	(*f)(name); */
+/*} */
+
+
 #include "function_pointers.h"
 
 /**
- * print_name - passes the name to another function to be printed
- * @name: the name to be printed
- * @f: the function to pass the name to.
- */
+  * print_name - Printing my name
+  * @name: First argument
+  * @f: Inner function
+  *
+  */
 
 void print_name(char *name, void (*f)(char *))
 {
 	if (name == NULL || f == NULL)
 		return;
 
-	/* if (!name || !f) */
-	/*	return; */
-
-	(*f)(name);
+	f(name);
 }
-
-
-
