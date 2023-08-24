789590872c50512e426b32a9786ed2a2ac3fe8b4
DegreesK0 <kelvinbora@gmail.com>
2023-08-24 22:39:47 +0000

    Upload

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void print_before_main(void) __attribute__((constructor));
+
+/**
+ * print_first - Prints a message before the main function is executed.
+ */
+void print_first(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
