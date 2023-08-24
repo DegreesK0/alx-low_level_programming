406336364bba2325828d42e019a37394b81ef329
DegreesK0 <kelvinbora@gmail.com>
2023-08-24 19:51:23 +0000

    Upload

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -15,10 +15,13 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 
+	if (new_node == NULL)
+		return (NULL);
 	if (str == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
+	if (str)
 	new_node->len = strlen(str);
 	new_node->next = *head;
 
