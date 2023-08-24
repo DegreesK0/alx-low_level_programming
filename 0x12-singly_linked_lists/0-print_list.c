5b63798eaeae5090a72485d2219c607426a978d9
DegreesK0 <kelvinbora@gmail.com>
2023-08-24 18:51:14 +0000

    Upload

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_list - prints all the elements of a linked list
+ * @h: current position in the linked list
+ *
+ * Return: the number of nodes
+ */
+
+size_t print_list(const list_t *h)
+{
+	/* struct list_s *current; */
+	int num_nodes = 0;
+
+	while (h != NULL)
+	{
+		printf("[%d] ", h->len);
+		if (h->str == NULL)
+			printf("(nil) ");
+		else
+			printf("%s", h->str);
+		printf("\n");
+		num_nodes++;
+		h = h->next;
+	}
+
+	return (num_nodes);
+}
