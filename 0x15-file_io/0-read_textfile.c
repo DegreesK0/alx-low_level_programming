f1a416edfeab5f59ba150b3f208b41eea0a4c2e7
DegreesK0 <kelvinbora@gmail.com>
2023-09-04 20:28:24 +0000

    Task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/* #include <stddef.h> */
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+/**
+ * read_textfile - reads a textfile and writes the output to POSIX (terminal)
+ * @filename: name of the file being read
+ * @letters: length of the file being read
+ *
+ * Return: 0 if there is an error, the number of letters read and printed
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	/* FILE *fp; */
+	/* char ch[10000]; */
+	/* fp = fopen(filename, "r"); */
+	/* printf("%s", fgets(ch, letters, fp)); */
+
+	/* fclose(fp); */
+	/* return (letters); */
+
+	int fp;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
+		return (0);
+
+	fp = open(filename, O_RDONLY);
+	/* if (fp == NULL) */
+	/*	return (0); */
+
+	read(fp, buffer, letters);
+	printf("%s\n", buffer);
+	free(buffer);
+	close(fp);
+
+	return (letters);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,26 @@
+#ifndef STDLIB_H
+#define STDLIB_H
+#include <stdlib.h>
+#endif
+
+#ifndef UNISTD_H
+#define UNISTD_H
+#include <unistd.h>
+#endif
+
+#ifndef STDDEF_H
+#define STDDEF_H
+#include <stddef.h>
+#endif
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
+
+
+
+#endif
