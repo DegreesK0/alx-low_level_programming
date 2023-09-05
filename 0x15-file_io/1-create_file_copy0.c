db5bb1cafd72ed9c9260a165ee20ce05a7795650
DegreesK0 <kelvinbora@gmail.com>
2023-09-05 08:47:20 +0000

    Upload

--- 0x15-file_io/1-create_file_copy0.c
+++ 0x15-file_io/1-create_file_copy0.c
@@ -27,8 +27,11 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 
 	len = 0;
-	while (text_content[len])
+	if (text_content != NULL)
+	{
+		while (text_content[len])
 		len++;
+	}
 	/* buffer = malloc(sizeof(text_content)); */
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
