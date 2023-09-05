ea68bfe4cfe14ae5faade1184c41e58756483690
DegreesK0 <kelvinbora@gmail.com>
2023-09-05 08:21:27 +0000

    Upload

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,28 +9,32 @@
 
 
 /**
- * create_file - a
- * @filename: a
- * @text_content: a
+ * create_file - creates a file
+ * @filename: name of file to be created
+ * @text_content: text to be written to the file
  *
- * Return: a
+ * Return: 1 on success, -1 on failure
  */
 
 int create_file(const char *filename, char *text_content)
 {
-	int fd, w, len = 0;
+	int fd;
+	/* char *buffer; */
+	int bytes_written;
 
 	if (filename == NULL)
 		return (-1);
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-		len++;
-	}
+
+	/* buffer = malloc(sizeof(text_content)); */
+
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(fd, text_content, len);
-	if (fd == -1 || w == -1)
+	bytes_written = write(fd, text_content, strlen(text_content));
+	if (bytes_written == -1 || fd == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 	close(fd);
+
 	return (1);
 }
